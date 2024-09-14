aec14f1a90592caaf82cbf76e0d8685ffe205109
Marijn Haverbeke <marijn@haverbeke.berlin>
2024-09-14 15:36:22 +0200

    Add a Alt-Backtick as binding for startCompletion on macOS
    FIX: Add Alt-Backtick as additional binding on macOS, where IME can take over
    Ctrl-Space.
    
    Closes https://github.com/codemirror/dev/issues/1439

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ export function autocompletion(config: CompletionConfig = {}): Extension {
 
 /// Basic keybindings for autocompletion.
 ///
-///  - Ctrl-Space: [`startCompletion`](#autocomplete.startCompletion)
+///  - Ctrl-Space (and Alt-\` on macOS): [`startCompletion`](#autocomplete.startCompletion)
 ///  - Escape: [`closeCompletion`](#autocomplete.closeCompletion)
 ///  - ArrowDown: [`moveCompletionSelection`](#autocomplete.moveCompletionSelection)`(true)`
 ///  - ArrowUp: [`moveCompletionSelection`](#autocomplete.moveCompletionSelection)`(false)`
@@ -42,6 +42,7 @@ export function autocompletion(config: CompletionConfig = {}): Extension {
 ///  - Enter: [`acceptCompletion`](#autocomplete.acceptCompletion)
 export const completionKeymap: readonly KeyBinding[] = [
   {key: "Ctrl-Space", run: startCompletion},
+  {mac: "Alt-`", run: startCompletion},
   {key: "Escape", run: closeCompletion},
   {key: "ArrowDown", run: moveCompletionSelection(true)},
   {key: "ArrowUp", run: moveCompletionSelection(false)},
